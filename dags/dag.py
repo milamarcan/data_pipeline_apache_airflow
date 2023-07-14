fcf20468d6abd3e6ad4963acad381de4cb41ebe7
Mila Marcan <mila.marcan21@gmail.com>
2023-07-14 20:07:49 +0200

    fix: rename dag file

---
+++ dags/dag.py
@@ -0,0 +1,213 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                               LoadDimensionOperator, DataQualityOperator)
+from helpers import SqlQueries
+
+# AWS_KEY = os.environ.get('AWS_KEY')
+# AWS_SECRET = os.environ.get('AWS_SECRET')
+
+create_tables = """
+        CREATE TABLE IF NOT EXISTS public.artists (
+        artistid varchar(256) NOT NULL,
+        name varchar(256),
+        location varchar(256),
+        lattitude numeric(18,0),
+        longitude numeric(18,0)
+    );
+
+    CREATE TABLE IF NOT EXISTS public.songplays (
+        playid varchar(32) NOT NULL,
+        start_time timestamp NOT NULL,
+        userid int4 NOT NULL,
+        "level" varchar(256),
+        songid varchar(256),
+        artistid varchar(256),
+        sessionid int4,
+        location varchar(256),
+        user_agent varchar(256),
+        CONSTRAINT songplays_pkey PRIMARY KEY (playid)
+    );
+
+    CREATE TABLE IF NOT EXISTS public.songs (
+        songid varchar(256) NOT NULL,
+        title varchar(256),
+        artistid varchar(256),
+        "year" int4,
+        duration numeric(18,0),
+        CONSTRAINT songs_pkey PRIMARY KEY (songid)
+    );
+
+    CREATE TABLE IF NOT EXISTS public.staging_events (
+        artist varchar(256),
+        auth varchar(256),
+        firstname varchar(256),
+        gender varchar(256),
+        iteminsession int4,
+        lastname varchar(256),
+        length numeric(18,0),
+        "level" varchar(256),
+        location varchar(256),
+        "method" varchar(256),
+        page varchar(256),
+        registration numeric(18,0),
+        sessionid int4,
+        song varchar(256),
+        status int4,
+        ts int8,
+        useragent varchar(256),
+        userid int4
+    );
+
+    CREATE TABLE IF NOT EXISTS public.staging_songs (
+        num_songs int4,
+        artist_id varchar(256),
+        artist_name varchar(256),
+        artist_latitude numeric(18,0),
+        artist_longitude numeric(18,0),
+        artist_location varchar(256),
+        song_id varchar(256),
+        title varchar(256),
+        duration numeric(18,0),
+        "year" int4
+    );
+
+    CREATE TABLE IF NOT EXISTS public."time" (
+        start_time timestamp NOT NULL,
+        "hour" int4,
+        "day" int4,
+        week int4,
+        "month" varchar(256),
+        "year" int4,
+        weekday varchar(256),
+        CONSTRAINT time_pkey PRIMARY KEY (start_time)
+    );
+
+    CREATE TABLE IF NOT EXISTS public.users (
+        userid int4 NOT NULL,
+        first_name varchar(256),
+        last_name varchar(256),
+        gender varchar(256),
+        "level" varchar(256),
+        CONSTRAINT users_pkey PRIMARY KEY (userid)
+    );
+    """
+
+default_args = {
+    'owner': 'milamarcan',
+    'start_date': datetime(2023, 1, 12),
+    "depends_on_past": False,
+    "retries": 3,
+    "retry_delay": timedelta(minutes=5),
+    "catchup": False,
+    "email_on_retry": False
+}
+
+dag = DAG('udacity_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='0 * * * *'
+          )
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+create_tables = PostgresOperator(
+    task_id="Create_tables",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=create_tables
+)
+
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    table="staging_events",
+    s3_bucket="",
+    s3_key="",
+    json_format=''
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    table="staging_songs",
+    s3_bucket="",
+    s3_key="",
+    json_format='auto'
+)
+
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="songplays",
+    sql_query=SqlQueries.songplay_table_insert
+)
+
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='Load_user_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="users",
+    sql_query=SqlQueries.user_table_insert,
+    append_mode=True,
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id='Load_song_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="songs",
+    sql_query=SqlQueries.song_table_insert,
+    append_mode=True,
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id='Load_artist_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="artists",
+    sql_query=SqlQueries.artist_table_insert,
+    append_mode=False,
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id='Load_time_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="time",
+    sql_query=SqlQueries.time_table_insert,
+    append_mode=True,
+)
+
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag,
+    redshift_conn_id="redshift",
+    tables=["users", "songs", "artists", "time", "songplays"]
+)
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+
+start_operator >> create_tables
+create_tables >> stage_events_to_redshift
+create_tables >> stage_songs_to_redshift
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+load_user_dimension_table >> run_quality_checks
+load_song_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+run_quality_checks >> end_operator
